e195d1eacc310dc0c73bd99cc4b0dfbd9e98ce74
jakubpoltoraczyk <jakubpoltoraczyk@onet.pl>
2020-05-07 16:12:30 +0200

    Dron

--- main.cpp
+++ main.cpp
@@ -6,15 +6,30 @@
 int main()
 {
     using namespace std;
-    std::ifstream plik;
-    plik.open("plik");
+    std::ifstream plik_drone,plik_boxes;
+    plik_drone.open("plik_drone");
+    plik_boxes.open("plik_boxes");
     Vector <double,3> vec[8];
     Matrix <double,3> pom_mat;
     for(int i=0;i<8;++i)
-        plik >> vec[i];
-    plik >> pom_mat; 
+        plik_drone >> vec[i];
+    plik_drone >> pom_mat; 
     drawNS::APIGnuPlot3D * api_gnu = new drawNS::APIGnuPlot3D(-100,100,-100,100,-100,100,-1);
     Drone dron01(vec,Vector<double,3>(),pom_mat,api_gnu);
+    for(int i=0;i<8;++i)
+        plik_boxes >> vec[i];
+    Cuboid box01(vec,Vector<double,3>(),pom_mat,api_gnu);
+    for(int i=0;i<8;++i)
+        plik_boxes >> vec[i];
+    Cuboid box02(vec,Vector<double,3>(),pom_mat,api_gnu);
+    cout << "Podaj punkt srodkowy (x,y,z) pudelka nr 1:\n";
+    cin >> vec[0];
+    cout << "Podaj punkt srodkowy (x,y,z) pudelka nr 2:\n";
+    cin >> vec[1];
+    box01.draw();
+    box01.replace(vec[0]);
+    box02.draw();
+    box02.replace(vec[1]);
     dron01.draw();
     for(;;)
     {
